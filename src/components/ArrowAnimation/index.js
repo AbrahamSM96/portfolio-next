68fb5d1a66d0bbb815181f47afe53da19f39cbf7
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-04-26 12:26:23 -0500

    FEAT: colors arrow

--- src/components/ArrowAnimation/index.js
+++ src/components/ArrowAnimation/index.js
@@ -1,14 +1,18 @@
 import styles from "./ArrowAnimation.module.css";
+import { useRouter } from "next/router";
 
 export default function ArrowAnimation({ route, style }) {
+  const router = useRouter();
+  const PATH = router.pathname;
+  const styleHome = PATH === "/" ? "transparent" : "var(--arrow)";
   return (
     <>
       <div className={styles._content_button} style={style}>
-        <button className={styles._button} onClick={route} name="scroll">
+        <div className={styles._button} onClick={route} name="scroll">
           <div className={styles._scrolldown}>
-            <span />
+            <span style={{ background: styleHome }} />
           </div>
-        </button>
+        </div>
       </div>
     </>
   );
