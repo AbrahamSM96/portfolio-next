244d012d9e16f2e2adeca8df54596bbfcda13d6b
Abraham Serrano <abrahamserrano96@hotmail.com>
2022-04-26 12:26:02 -0500

    DELETE: code unnecessary

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { FaHeart } from "react-icons/fa";
 import styles from "./Footer.module.css";
+
 export default function Footer() {
   const refDate = useRef(null);
+
   useEffect(() => {
     const date = new Date().getFullYear();
     refDate.current.append(date);
   }, []);
-
   return (
     <>
       <footer className={styles._footer}>
